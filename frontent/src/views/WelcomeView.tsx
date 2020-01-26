c8eb3a2357af2be8f3d99ba02e266fa4d9130dae
Paul Biberstein <biberstein.paul@gmail.com>
2020-01-26 10:19:04 -0500

    Removed debug button

--- frontent/src/views/WelcomeView.tsx
+++ frontent/src/views/WelcomeView.tsx
@@ -31,9 +31,6 @@ export class WelcomeView extends React.Component<IProps> {
                         <div className="button" onClick={() => store.PageController.ToLocationSearch()}>
                             By Location
                         </div>
-                        <div className="button" onClick={() => store.PageController.ToHaikuDisplay([""], [[""]])}>
-                            Debug
-                        </div>
                     </span>
                 </div>
             </div>
