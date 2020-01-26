ab4ccebd3aa21562098dedcc32a5c8b77e2b6322
OceanPak <21999617+OceanPak@users.noreply.github.com>
2020-01-25 21:38:28 -0500

    backend files initial commit

---
+++ backend/app.py
@@ -0,0 +1,103 @@
+from flask import Flask, render_template, request, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, validators
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
+import wikipedia
+import requests
+from operator import itemgetter
+import syllables
+from nltk.tokenize import sent_tokenize, word_tokenize
+import string
+from test import rap, fetch_page_content, clean_article
+import time
+from flask import jsonify
+
+"""
+views = []
+for i in results:
+    URL = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/all-agents/{}/monthly/2019120100/2019123100".format(i)
+    r = requests.get(url = URL)
+    data = r.json()
+    # could be empty
+    views.append((data['items'][0]['article'], data['items'][0]['views']))
+m = max(views, key=itemgetter(1))[0]
+#print(m)
+#print(wikipedia.page(m).summary)
+a = sent_tokenize(wikipedia.page(m).summary)
+print(rap(a))
+"""
+
+"""
+wiki_page = "https://en.wikipedia.org/wiki/Brown_University"
+title, page = fetch_page_content(wiki_page)
+title = clean_article(title)
+sentences = [clean_article(sent).strip() for sent in sent_tokenize(page)]
+couplets = rap(sentences)
+print(couplets)
+"""
+
+#a = [''.join(c for c in s if c not in string.punctuation) for s in a]
+#a = [s for s in a if s]
+#a = [s for s in a if len(s) > 1]
+#print(a)
+"""
+syl = 0
+words = ""
+for i in a:
+    if words != "":
+        words = words + " "
+        words += i
+    else:
+        words += i
+    currentSyl = syllables.estimate(words)
+    if currentSyl > 7:
+        print(words)
+        print("\n")
+        syl = 0
+        words = """""
+
+app = Flask(__name__)
+Bootstrap(app)
+
+# This is for the form submission, don't worry about it being secure.
+#app.config['SECRET_KEY'] = 'penguinsrulelots'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    #if request.args.get('source') == 'word':
+    #else if request.args.get('source') == 'location':
+    return "<p1>hello</p1>"
+
+@app.route('/ArticleTitles', methods=['GET', 'POST'])
+def article():
+    if request.args.get('source') == 'text':
+        text = request.args.get('data')
+        #text = "Python"
+        #start = time.time()
+        mispelled = wikipedia.suggest(text)
+        if mispelled:
+            results = wikipedia.search(mispelled)
+            #print(results)
+        else:
+            results = wikipedia.search(text)
+            #print(results)
+        #print(results)
+        results = (list(set(results)))
+        return jsonify(listOfTitles=results)
+    #else if request.args.get('source') == 'location':
+
+@app.route('/GenerateHaiku', methods=['GET', 'POST'])
+def generateHaiku():
+    title = request.args.get('articletitle')
+    #print(results)
+    wiki_page = wikipedia.page(title).url
+    title, page = fetch_page_content(wiki_page)
+    title = clean_article(title)
+    sentences = [clean_article(sent).strip() for sent in sent_tokenize(page)]
+    couplets = rap(sentences)
+    return jsonify(couplets=couplets)
+    #end = time.time()
+    #print(end - start)
+    
+app.run(debug=True)
---
+++ backend/test.py
@@ -0,0 +1,174 @@
+MAX_LINE_LENGTH = 105
+USE_MORE_MAX = 1
+NUM_PERFECT = 500
+NUM_IMPERFECT = 500
+THREAD_WAIT = 0.03
+from time import sleep
+from datamuse import datamuse
+from nltk.tokenize import sent_tokenize, word_tokenize
+from threading import Thread
+import wikipedia
+
+import lxml
+import bs4
+import urllib
+
+import re
+
+def clean_article(article):
+    article = re.sub(r"\(.+\)", "", article)
+    article = re.sub(r"\[.+\]", "", article)
+    article = re.sub(r":\d+\s", "", article)
+    article = article.replace('"', "")
+    article = re.sub(r"\<\w+\>", "", article)
+    article = re.sub(r"\<\/\w+\>", "", article)
+
+    return article
+
+
+def fetch_page_content(url):
+    data = urllib.request.urlopen(url)
+    content = data.read()
+    parsed_content = bs4.BeautifulSoup(content, "lxml")
+    div = parsed_content.find("h1", id="firstHeading")
+    title = "".join(map(str, div.contents))
+    paragraphs = parsed_content.find_all("p")
+    article_text = ""
+    for p in paragraphs:
+        article_text += p.text
+    return title, article_text.replace("\n", " ")
+
+
+def get_end_words(sentences):
+    ending_words = []
+    for s in sentences:
+        words = word_tokenize(s)
+        a = words.pop()
+        if words:
+            end = words.pop()
+            ending_words.append(end.lower())
+        else:
+            ending_words.append(a)
+    return ending_words
+
+
+def thread_perf(word1, x, ending_words, output, seen, api, used, max=NUM_PERFECT):
+    if word1 in seen:
+        #print("SAVED")
+        return
+    perf = api.words(rel_rhy=word1, max=NUM_PERFECT)
+    rhymes_here = []
+    for entry in perf:
+        w = entry["word"]
+        for y, e in enumerate(ending_words[x:]):
+            if w == e:
+                rhymes_here.append((w, y + x))
+                if (word1, w) not in used and (w, word1) not in used:
+                    #print(word1, w, entry["score"])
+                    used.add((word1, w))
+                    output.append((word1, w, x, y + x))
+                    seen.add(word1)
+                    seen.add(w)
+    for j in range(len(rhymes_here)):
+        for k in range(j + 1, len(rhymes_here)):
+            if (rhymes_here[j][0], rhymes_here[k][0]) not in used and rhymes_here[j][
+                0
+            ] != rhymes_here[k][0]:
+                used.add((rhymes_here[j][0], rhymes_here[k][0]))
+                output.append(
+                    (
+                        rhymes_here[j][0],
+                        rhymes_here[k][0],
+                        rhymes_here[j][1],
+                        rhymes_here[k][1],
+                    )
+                )
+                #print((rhymes_here[j][0], rhymes_here[k][0]))
+
+
+def thread_almost(word1, x, ending_words, output, seen2, api, used, max=NUM_IMPERFECT):
+    if word1 in seen2:
+        #print("SAVED")
+        return
+    almost = api.words(rel_nry=word1, max=NUM_IMPERFECT)
+    # rhymes_here = []
+
+    for entry in almost:
+        w = entry["word"]
+        for y, e in enumerate(ending_words[x:]):
+            if w == e:
+                # rhymes_here.append((w, y + x))
+                if (word1, w) not in used and (w, word1) not in used:
+                    #print(word1, w, entry["score"])
+                    used.add((word1, w))
+                    output.append((word1, w, x, y + x))
+                    seen2.add(word1)
+                    seen2.add(w)
+
+
+def rap(sentences):
+    used = set()
+    rhymes = []
+    api = datamuse.Datamuse()
+    for s in sentences:
+        if not s:
+            sentences.remove(s)
+    ending_words = get_end_words(sentences)
+    seen = set()
+    threads = [None] * len(ending_words)
+    results = [[] for _ in range(len(ending_words))]
+    for x, word1 in enumerate(ending_words):
+        threads[x] = Thread(
+            target=thread_perf,
+            args=(word1, x, ending_words, results[x], seen, api, used),
+        )
+        threads[x].start()
+        sleep(THREAD_WAIT)
+
+    for i in range(len(threads)):
+        threads[i].join()
+    for r in results:
+        if r:
+            for e in r:
+                rhymes.append(e)
+    #print(rhymes)
+
+    seen2 = set()
+    if len(rhymes) < USE_MORE_MAX:
+        #print("USING MORE")
+        threads2 = [None] * len(ending_words)
+        results2 = [[] for _ in range(len(ending_words))]
+        for x, word1 in enumerate(ending_words):
+            threads2[x] = Thread(
+                target=thread_almost,
+                args=(word1, x, ending_words, results2[x], seen2, api, used),
+            )
+            threads2[x].start()
+            sleep(THREAD_WAIT)
+
+        for i in range(len(threads2)):
+            threads2[i].join()
+        for r in results2:
+            if r:
+                for e in r:
+                    rhymes.append(e)
+
+    #print(rhymes)
+
+    rhymes.sort(key=lambda x: abs(x[2] - x[3]))
+    rhymes.sort(key=lambda x: x[2])
+
+    couplets = []
+
+    for r in rhymes:
+        couplets.append((sentences[r[2]].replace(u'\xa0', u' '), sentences[r[3]].replace(u'\xa0', u' ')))
+
+    # couplets.sort(key=lambda x: abs(len(x[0]) - len(x[1])))
+    # couplets.sort(key=lambda x: len(x[0]))
+    couplets = [
+        c
+        for c in couplets
+        if len(c[0]) < MAX_LINE_LENGTH and len(c[1]) < MAX_LINE_LENGTH
+    ]
+    return couplets
+
